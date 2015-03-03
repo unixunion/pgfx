c05a8e7fcf258efaf0ec620f10b7d918f1c06fc1
Kegan Holtzhausen <keghol@surfacepro3.qeclient.local>
2015-03-03 15:30:08 +0000

    ported over to u8glib, saves memory

--- pgfx.h
+++ pgfx.h
@@ -1,25 +1,32 @@
 #ifndef _PSIMAX_GFX_H
 #define _PSIMAX_GFX_H
 
-/* EXAMPLE
- * pgfx_HBAR fbar = pgfx_HBAR(0, 0,20,64,"FUEL",display);
- * pgfx_HBAR mbar = pgfx_HBAR(32,0,20,64,"MONO",display);
- * pgfx_HBAR gbar = pgfx_HBAR(64, 0,20,64,"FUEL",display);
- * pgfx_HBAR hbar = pgfx_HBAR(96,0,20,64,"MONO",display);
- * // single bargraphs
- * fbar.update(32,100);
- * mbar.update(100,100);
- * // dual bargraphs
- * gbar.update(32,100,100,100);
- * hbar.update(4,100,10,100);
- * display.display();
+/* 
+
+https://code.google.com/p/u8glib/
+
+
+// EXAMPLE
+// Gauges
+pgfx_HBAR fbar = pgfx_HBAR( 0, 0,20,64,"FUEL",display);
+pgfx_HBAR mbar = pgfx_HBAR(26, 0,20,64,"MONO",display);
+pgfx_HBAR ebar = pgfx_HBAR(52, 0,20,64,"ELEC",display);
+pgfx_HBAR gbar = pgfx_HBAR(78, 0,20,64,"G's",display);
+
+// single bargraphs
+fbar.update(VData.LiquidFuelS, VData.LiquidFuelTotS, VData.LiquidFuel, VData.LiquidFuelTot);
+mbar.update(VData.MonoProp, VData.MonoPropTot);
+ebar.update(VData.ECharge, VData.EChargeTot);
+gbar.update(VData.G, 15);
+display.display();
+
 */
 
-#include <Adafruit_SSD1306.h>
+#include <U8glib.h>
 
 class pgfx_HBAR {
 public:
-	pgfx_HBAR(int x, int y, int w, int h, char * name, Adafruit_SSD1306 &display); // Constructor
+	pgfx_HBAR(int x, int y, int w, int h, char * name, U8GLIB_SSD1306_128X64 &display); // Constructor
 
 	void
 		update(float value, float max),
@@ -29,7 +36,7 @@ private:
 	int
 		_width, _height,
 		_x, _y;
-	Adafruit_SSD1306 * _display;
+	U8GLIB_SSD1306_128X64 * _display;
 	char * _name;
 };
 
