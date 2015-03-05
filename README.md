0cb8d373890d12939b11895a6dd6103bea068d14
Kegan Holtzhausen <keghol@STHMACLT034.local>
2015-03-05 10:48:12 +0000

    cleaned up and readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # pgfx
+
+simple bar gauges for KSP Serial IO. Depends on u8glib
--- pgfx.cpp
+++ pgfx.cpp
@@ -14,15 +14,11 @@ void pgfx_HBAR::update(float value, float max)
 {
 
   _display->drawStr(_x, _y, _name);
-  // _display->setTextSize(1);
-  // _display->setTextColor(WHITE);
   _display->drawRFrame(_x, _y+16, _width, _height-16, 4);
   if (max >0 && value > 1) {
   	unsigned int fp = (value / max) * (_height-20);
   	_display->drawBox(_x+2, _y+18 + ((_height-20) - fp), _width-4, fp);
   }
-  // _display->setCursor(_x,_y);
-  // _display->println(_name);
 
 }
 
@@ -30,8 +26,6 @@ void pgfx_HBAR::update(float v1, float m1, float v2, float m2)
 {
   _display->drawStr(_x, _y, _name);
 
-  // _display->setTextSize(1);
-  // _display->setTextColor(WHITE);
   _display->drawRFrame(_x, _y+16, _width, _height-16, 4);
   if (m1 > 0 && v1 > 1) {
   	unsigned int fp1 = (v1 / m1) * (_height-20);
@@ -41,8 +35,6 @@ void pgfx_HBAR::update(float v1, float m1, float v2, float m2)
   	unsigned int fp2 = (v2 / m2) * (_height-20);
   	_display->drawBox(_x+(_width/2)+1, _y+18 + ((_height-20) - fp2), (_width/2)-3, fp2);
   }
-  // _display->setCursor(_x,_y);
-  // _display->println(_name);
 
 }
  
